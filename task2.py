df44cae612f2b4c925b901aff7e12dba77a3c533
Jawad Amjad <144198102+jawad6555@users.noreply.github.com>
2023-10-18 10:42:57 +0500

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,7 @@
+
+functionsOflist = ["Sort()","Type()","Append()","Extend()","Index()"] 
+functionsOfdictionary = ["any()","len()","cmp()","sorted()"] 
+functionsOfset = ["len", "max", "min"] 
+functionsOftuple = ["len", "max", "min","sum()","count"] 
+thisdict = { "List Functions":functionsOflist, "Dictionary Functions":functionsOfdictionary, "Set Functions":functionsOfset, "Tuple Functions":functionsOftuple} 
+print(thisdict)
